2c53a4a954e4696dc08e811af9ad3ccf79e942b9
Jardel Torres <74869343+jardimdanificado@users.noreply.github.com>
2023-05-09 07:37:06 -0300

    update osga.lua

--- osga.lua
+++ osga.lua
@@ -1,44 +1,9 @@
---[[
-
-speed = 10 --iterations, bigger the speed slower it will be
-. = empty space
-a-Z = workers
-all workers must have their defaults
-
-signals:
-  * = power, always true, when it touch a worker run it with the defaults 
-  $ = setup, only data, this override the worker defaults
-  @ = transmission, this do not override the worker defaults, but run a single time the data in the worker
-
-signal operators: -- operators are pre-built logic controlers 
-  ! = outputer
-  # = blocker
-  
-special(need outputs): -- special are made with api functions
-  ? = redirects a signal
-  & = queue
-  + = send the signal in the 3 other directions
-  < = slow down by 1
-  > = speed up by 1
---]]
-
-local cfgtxt = 
-[[local cfg = {}
-cfg.size = 
-{
-  w = 16,
-  h = 16
-}
-cfg.name = "osga"
-return cfg]]
-
 local util = require("util")
 local api = require("api")
 
 local install = function()
   os.execute(util.unix("rm -rf","rd /s /q") .. " data")
   os.execute("mkdir data")
-  util.file.save.text("./data/config.lua",cfgtxt)
   local text = "local ruleset = {}\nruleset.defaults = {}\n\n"
   for k,v in pairs(util.char) do
     text = text .. "ruleset.defaults['" .. v .. "']" .. " = {}\n"
@@ -85,7 +50,7 @@ end
 local function commander(world)
   local cmd = io.read()
   if cmd == "exit" then
-    world.config.exit = true
+    world.session.exit = true
   end
 end
 
@@ -128,7 +93,7 @@ local function main()
   world.map.char = util.file.load.charMap("./data/map.txt")
   world.map.signal = util.matrix.new(16,16,'.')
   world.session.editmode = true
-  while not world.config.exit do
+  while not world.session.exit do
     frame(world)
     print_map(world)
     
