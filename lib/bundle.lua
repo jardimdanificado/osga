242cb77c327168ed49752efbb7124dc523a36299
Jardel Torres <74869343+jardimdanificado@users.noreply.github.com>
2023-05-21 03:24:01 -0300

    update bundle.lua

--- lib/bundle.lua
+++ lib/bundle.lua
@@ -1,7 +1,32 @@
 local ruleset = {command={},worker={color={},speed={}},signal={}}
 
 command.pack = function(world,api,args)
-	
+    local scripts = {}
+    local mapfile = ""
+    local outfile = ""
+	for i,v in ipairs(args) do
+	    if api.util.string.includes(v,".osgs") then
+	        table.insert(scripts,v)
+	    elseif api.util.string.includes(v,".osgm") then
+	        mapfile = v
+	    elseif api.util.string.includes(v,".osgb") then
+	        outfile = v
+	    end
+	end
+	if outfile == "" then
+	    outfile = api.util.string.replace(mapfile,".osgm",".osgb")
+	end
+	local maptxt = api.util.file.load.text(mapfile)
+	local map = api.util.file.load.charmap(mapfile)
+	local key = api.util.id()
+	while api.util.string.includes(maptxt, key) do 
+	    key = api.util.id()
+	end
+	local result = key .. "\n" .. maptxt .. key
+	for i,v in ipairs(scripts) do 
+	    result = result .. api.util.file.load.text(v)
+	end
+	api.util.file.save.text(outfile,result)
 end
 
 return ruleset
